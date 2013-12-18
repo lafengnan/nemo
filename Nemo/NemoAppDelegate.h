6ec400331f46f151d002781737b2b3beb84c76c5
lafengnan <panzhongbin@gmail.com>
2013-12-18 22:22:11 +0800

    Initial Commit

---
+++ Nemo/NemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NemoAppDelegate.h
+//  Nemo
+//
+//  Created by lafengnan on 13-12-18.
+//  Copyright (c) 2013年 panzhongbin@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nemo
+//
+//  Created by lafengnan on 13-12-18.
+//  Copyright (c) 2013年 panzhongbin@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NemoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NemoAppDelegate class]));
+    }
+}
